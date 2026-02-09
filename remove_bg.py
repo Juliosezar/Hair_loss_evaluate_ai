983beed16208813210865625ca4131b94ec7fab0
Sezar <julio.sezar@gmail.com>
2026-02-09 16:22:55 +0330

    feat: add "remove background" feature

---
+++ remove_bg.py
@@ -0,0 +1,24 @@
+import os
+import sys
+
+input_dir = './dataset/1- raw/'
+output_dir = './dataset/2- remove-bg/'
+
+
+from rembg import remove
+
+for i in os.listdir(input_dir):
+    try:
+        print(i)
+        input_path = os.path.join(input_dir, i)
+        output_path = os.path.join(output_dir, i)
+        
+
+        with open(input_path, 'rb') as i:
+            with open(output_path, 'wb') as o:
+                input = i.read()
+                output = remove(input)
+                o.write(output)
+    except:
+        print('error')
+        continue
